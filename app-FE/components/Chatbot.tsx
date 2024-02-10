4e2e2a2d90a4052446ad2beb669ac4fa610c0153
Vincent Nguyen <vincent.nguyen296@gmail.com>
2024-02-10 14:32:40 -0600

    front end chatbot

---
+++ app-FE/components/Chatbot.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Chatbot from 'react-chatbot-kit'
+import 'react-chatbot-kit/build/main.css'
+import config from '../Chatbot Dependencies/config'
+
+const FlightChatbot: React.FC = () => {
+    return (
+        <div>
+            <h1>This is the chatbot</h1>
+        </div>
+    )
+}
+export default FlightChatbot
---
+++ app-FE/public/ServiceWorker.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ServicerWorker() {
+    return (
+        <div>
+            alert('Service Worker Registered')
+        </div>
+    )
+}
+
+export default ServiceWorker
